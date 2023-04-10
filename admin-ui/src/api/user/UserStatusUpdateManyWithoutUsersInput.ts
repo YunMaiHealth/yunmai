0ad84098a3cedb772b5d4ad46755fb3e4d3cbc59
caoj81 <125623540+caoj81@users.noreply.github.com>
2023-04-10 15:04:09 +0800

    Delete UserStatusUpdateManyWithoutUsersInput.ts
    删除残留文件

--- admin-ui/src/api/user/UserStatusUpdateManyWithoutUsersInput.ts
+++
@@ -1,7 +0,0 @@
-import { UserStatusWhereUniqueInput } from "../userStatus/UserStatusWhereUniqueInput";
-
-export type UserStatusUpdateManyWithoutUsersInput = {
-  connect?: Array<UserStatusWhereUniqueInput>;
-  disconnect?: Array<UserStatusWhereUniqueInput>;
-  set?: Array<UserStatusWhereUniqueInput>;
-};
