912b2b63f2ba48bb66125c96372ea84c953ab9c3
xsl4756 <347183895@qq.com>
2023-04-12 09:24:14 +0800

    添加新增用户方法，不需要token也能新增用户。

--- server/src/auth/auth.resolver.ts
+++ server/src/auth/auth.resolver.ts
@@ -6,10 +6,14 @@ import { GqlDefaultAuthGuard } from "./gqlDefaultAuth.guard";
 import { UserData } from "./userData.decorator";
 import { LoginArgs } from "./LoginArgs";
 import { UserInfo } from "./UserInfo";
+import { User } from "../user/base/User";
+import { CreateUserArgs } from "../user/base/CreateUserArgs";
+import { UserService } from "../user/user.service";
 
 @Resolver(UserInfo)
 export class AuthResolver {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService,
+     private readonly service: UserService) {}
   @Mutation(() => UserInfo)
   async login(@Args() args: LoginArgs): Promise<UserInfo> {
     return this.authService.login(args.credentials);
@@ -20,4 +24,13 @@ export class AuthResolver {
   async userInfo(@UserData() userInfo: UserInfo): Promise<UserInfo> {
     return userInfo;
   }
+
+  @Mutation(() => User)
+  async registerUser(@Args() args: CreateUserArgs): Promise<User> {
+    return await this.service.create({
+      ...args,
+      data: args.data,
+    });
+  }
+
 }
