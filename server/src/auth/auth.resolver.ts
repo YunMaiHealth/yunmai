79f3e17ed7f95fbc7e07d07049dd80eb6b5f9fb9
caoj81 <125623540+caoj81@users.noreply.github.com>
2023-04-17 15:39:37 +0800

    Update auth.resolver.ts
    添加新增用户方法，不需要token也能新增用户

--- server/src/auth/auth.resolver.ts
+++ server/src/auth/auth.resolver.ts
@@ -6,18 +6,27 @@ import { GqlDefaultAuthGuard } from "./gqlDefaultAuth.guard";
 import { UserData } from "./userData.decorator";
 import { LoginArgs } from "./LoginArgs";
 import { UserInfo } from "./UserInfo";
+import { User } from "../user/base/User";
+import { CreateUserArgs } from "../user/base/CreateUserArgs";
+import { UserService } from "../user/user.service";
 
 @Resolver(UserInfo)
 export class AuthResolver {
-  constructor(private readonly authService: AuthService) {}
+ constructor(private readonly authService: AuthService,
+     private readonly service: UserService) {}
   @Mutation(() => UserInfo)
   async login(@Args() args: LoginArgs): Promise<UserInfo> {
     return this.authService.login(args.credentials);
-  }
-
-  @Query(() => UserInfo)
-  @common.UseGuards(GqlDefaultAuthGuard, gqlACGuard.GqlACGuard)
+	@@ -20,4 +24,13 @@ export class AuthResolver {
   async userInfo(@UserData() userInfo: UserInfo): Promise<UserInfo> {
     return userInfo;
+  }
+
+  @Mutation(() => User)
+  async registerUser(@Args() args: CreateUserArgs): Promise<User> {
+    return await this.service.create({
+      ...args,
+      data: args.data,
+    });
   }
 }
