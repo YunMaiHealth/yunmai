7c94672177dc561ba2663e533de50bdfdce48fb2
caoj81 <125623540+caoj81@users.noreply.github.com>
2023-04-17 15:26:34 +0800

    Update message.resolver.base.ts
    添加查询消息信息方法getMessageInfo

--- server/src/message/base/message.resolver.base.ts
+++ server/src/message/base/message.resolver.base.ts
@@ -206,4 +206,51 @@ export class MessageResolverBase {
     }
     return result;
   }
+ 
+   @common.UseInterceptors(AclFilterResponseInterceptor)
+  @graphql.Query(() => [MessageInfo])
+  @nestAccessControl.UseRoles({
+    resource: "Message",
+    action: "read",
+    possession: "any",
+  })
+  async getMessageInfo(
+    @graphql.Args() args: MessageFindManyArgs
+  ): Promise<MessageInfo[]> {
+
+ const allMessages =await this.service.findMany(args);
+ const messageMap = new Map();
+ const newMessageMap = new Map();
+  for(let i=0;i<allMessages.length;i++){
+       //获取每个类型最新消息信息
+       let object = allMessages[i]
+       let type = object.messageType
+        if(messageMap.has(type)){
+          if(object.sendTime>messageMap.get(type).sendTime){
+            messageMap.set(type,allMessages[i])
+          }
+        }else{
+          messageMap.set(type,allMessages[i])
+        }
+       //获取每个类型未读消息数量
+       let isNew = object.isNew
+       if(isNew==true){
+          if(newMessageMap.has(type)){
+            newMessageMap.set(type,newMessageMap.get(type)+1)
+          }else{
+            newMessageMap.set(type,1)
+           }
+       }
+  }
+  //合并数据
+  let MessageNotify :MessageInfo[] = []
+  for (var value of messageMap.values()) {
+    if(newMessageMap.has(value.messageType)){
+      value.newMessageNum = newMessageMap.get(value.messageType)
+    }
+    MessageNotify.push(value)
+   }
+
+    return MessageNotify;
+  }
 }
